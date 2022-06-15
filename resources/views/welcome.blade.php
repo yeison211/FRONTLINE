3400f2b77ad7ed7dd4bde3a2773504a3711fb35a
Arturo Batista Quiroz <batistaquiroz@gmail.comm>
2022-06-15 17:09:09 -0500

    Actualizacion css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>FrontLine</title>
+    <link rel="stylesheet" href="{{asset('css/main.css')}}">
 </head>
 <body>
-    <h1>FrontLine</h1>
-    <nav>esto es un navbar</nav>
-    <footer>Soy un footer</footer>
+    <header></header>
+    <main></main>
+    <footer></footer>
 </body>
 </html>
