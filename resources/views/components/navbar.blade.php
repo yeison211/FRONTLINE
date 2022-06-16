eaca84ebacdfed9e6981c3e326ed5e151a00abe6
Arturo Batista Quiroz <batistaquiroz@gmail.comm>
2022-06-15 21:13:59 -0500

    Arreglado el problema con boostrap y el navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar">
+<nav class="navbar-pc">
     <ul>
-        <li class="navbar__logo" style="color: blue"><img src="{{asset('images/logo.jpg')}}" alt=""></li>
+        <li class="navbar-pc__logo" style="color: blue"><img src="{{asset('images/logo.jpg')}}" alt=""></li>
         <li class="nav-bar__toggle-btn"> <div class="toggle-btn__line"></div></li>
         <li class="item">Contactenos</li>
         <li class="item">Servicio</li>
