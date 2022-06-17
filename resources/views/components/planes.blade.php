1ce1994d621cb7e790fbfeff46522dcf5ca5c590
Carlosale19 <Carlosalejandromontalvo@gmail.com>
2022-06-16 22:27:27 -0500

    act caja planes

--- resources/views/components/planes.blade.php
+++ resources/views/components/planes.blade.php
@@ -1,14 +1,56 @@
 <section class="section-planes">
     <h2>Planes</h2>
     <div class="info-planes">
-        <div class="planes__item">
-
+        <div class="plan-box">
+            <div class="plan-option">
+                <p class="plan-name">
+                    Plan 1
+                </p>
+                <div class="plan-price">
+                    <p>99999$</p>
+                </div>
+                <ul class="plan-details">
+                    <li><strong>detalle</strong></li>
+                    <li><strong>detalle</strong></li>
+                    <li><strong>detalle</strong></li>
+                    <li><strong>detalle</strong></li>
+                </ul>
+                <a href="" class="plan-btn">Obtener ya</a>
+            </div>
         </div>
-        <div class="planes__item">
-
+        <div class="plan-box">
+            <div class="plan-option">
+                <p class="plan-name">
+                    Plan 1
+                </p>
+                <div class="plan-price">
+                    <p>99999$</p>
+                </div>
+                <ul class="plan-details">
+                    <li><strong>detalle</strong></li>
+                    <li><strong>detalle</strong></li>
+                    <li><strong>detalle</strong></li>
+                    <li><strong>detalle</strong></li>
+                </ul>
+                <a href="" class="plan-btn">Obtener ya</a>
+            </div>
         </div>
-        <div class="planes__item">
-
+        <div class="plan-box">
+            <div class="plan-option">
+                <p class="plan-name">
+                    Plan 1
+                </p>
+                <div class="plan-price">
+                    <p>99999$</p>
+                </div>
+                <ul class="plan-details">
+                    <li><strong>detalle</strong></li>
+                    <li><strong>detalle</strong></li>
+                    <li><strong>detalle</strong></li>
+                    <li><strong>detalle</strong></li>
+                </ul>
+                <a href="" class="plan-btn">Obtener ya</a>
+            </div>
         </div>
     </div>
 </section>
