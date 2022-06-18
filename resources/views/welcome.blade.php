28e68c113b6c69d1f1ae98b484ef3c9dc27580eb
Yeison <yeisondbv2020@gmail.com>
2022-06-17 23:40:30 -0500

    icono_frontline_title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>FrontLine</title>
+    <!--icono frontline-->
+    <link rel="shortcut icon" href="{{ asset('images/Frotnlinr-Azul.png') }}">
+    <!--icono frontline-->
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
     {{-- mapa --}}
