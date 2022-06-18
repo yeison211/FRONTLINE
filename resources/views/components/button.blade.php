398e1e60f7966dc1f8f53c1e8ce3d94ca4ca80a5
Yeison <yeisondbv2020@gmail.com>
2022-06-18 15:51:22 -0500

    actualiza whats

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,7 +2,7 @@
 <!-- button whatsapp  -->
 <body>
     <!--link de cuenta de whatsapp -asesora  -->
-<a href="https://api.whatsapp.com/send?phone=3217717983" class="button-what" target="_blank">
+<a href="https://api.whatsapp.com/send?phone=573217717983&text=Frontline%20%7CServicio%20Al%20Cliente%20%7CAsesora%20:Yarlenis" class="button-what" target="_blank">
     <img src="{{ asset('images/btn_whatsapp.png') }}" alt="">
 </a>
 </body>
