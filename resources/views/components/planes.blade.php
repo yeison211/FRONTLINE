c3e88e796c4048e0df27c5c66dfdecd413267fe7
Yeison Barrios <74212079+yeison211@users.noreply.github.com>
2022-12-19 14:13:15 -0500

    Update planes.blade.php
    assaas

--- resources/views/components/planes.blade.php
+++ resources/views/components/planes.blade.php
@@ -4,6 +4,26 @@
         <div class="plan-box">
             <div class="plan-option">
                 <h3 class="plan-name">
+                   10 MEGAS + TV
+                </h3>
+                <p class="plan-subtitulo">INTERNET</p>
+                <ul class="plan-details">
+                    <li><img src="{{asset('icons/infinity-solid.svg')}}" alt="" class=""> Ilimitado</li>
+                    <li><img src="{{asset('icons/speed.svg')}}" alt=""> Velocidad simetrica</li>
+                    <li><img src="{{asset('icons/fiber.svg')}}">Fibra optica</li>
+                    <li><img src="{{asset('icons/wifi.svg')}}"> Banda ancha</li>
+                </ul>
+                <p class="plan-subtitulo">TELEVISION</p>
+                <img src="{{asset('images/canales.png')}}" alt="">
+                <div class="plan-price">
+                    <div>Desde $55<span>Mil</span></div>
+                    <button class="btn1">LO QUIERO</button>
+                </div>
+            </div>
+        </div>
+        <div class="plan-box">
+            <div class="plan-option">
+                <h3 class="plan-name">
                    25 MEGAS + TV
                 </h3>
                 <p class="plan-subtitulo">INTERNET</p>
@@ -37,26 +57,6 @@
                 <img src="{{asset('images/canales.png')}}" alt="">
                 <div class="plan-price">
                     <div>Desde $75<span>Mil</span></div>
-                    <button class="btn1">LO QUIERO</button>
-                </div>
-            </div>
-        </div>
-        <div class="plan-box">
-            <div class="plan-option">
-                <h3 class="plan-name">
-                   100 MEGAS + TV
-                </h3>
-                <p class="plan-subtitulo">INTERNET</p>
-                <ul class="plan-details">
-                    <li><img src="{{asset('icons/infinity-solid.svg')}}" alt="" class=""> Ilimitado</li>
-                    <li><img src="{{asset('icons/speed.svg')}}" alt=""> Velocidad simetrica</li>
-                    <li><img src="{{asset('icons/fiber.svg')}}">Fibra optica</li>
-                    <li><img src="{{asset('icons/wifi.svg')}}"> Banda ancha</li>
-                </ul>
-                <p class="plan-subtitulo">TELEVISION</p>
-                <img src="{{asset('images/canales.png')}}" alt="">
-                <div class="plan-price">
-                    <div>Desde $95<span>Mil</span></div>
                     <button class="btn1">LO QUIERO</button>
                 </div>
             </div>  
